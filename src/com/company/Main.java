fde3246ff3ee3ae6c0e2008e67af77a5e2b3356d
aruzhanrakhimberdina <99319593+aruzhanrakhimberdina@users.noreply.github.com>
2022-02-24 23:48:01 +0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
     IUserRepository rep=new LoginRepository(db);
-    Controller contr=new Controller(rep);
-    Registration reg=new Registration(contr);
-    reg.loginStart();
+    Controller contr=new Controller(rep); //allocating memory for the class  Controller
+    Registration reg=new Registration(contr); //allocating memory for the class  Registration
+    reg.loginStart(); //starting the project
     }
 }
