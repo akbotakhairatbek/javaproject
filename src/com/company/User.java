e4ccc059c8c667145d7094ab77fb558a160a8e3f
akbota <97677267+akbotakhairatbek@users.noreply.github.com>
2022-02-24 23:53:46 +0600

    Update User.java

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -6,22 +6,23 @@ public class User {
 
     public User(String login, String password) {
         setLogin(login);
-        setPassword(password);
+        setPassword(password); 
+         //sets the login and password of the user
     }
     public String getLogin() {
         return login;
-    }
+    } 
 
     public void setLogin(String login) {
         this.login = login;
-    }
+    } 
 
     public String getPassword() {
-        return password;
+        return password; 
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
-
+//using this, it refers to the current object
 }
