c6b85aa74eb609ffc2608d1a00e4641d57930438
aruzhanrakhimberdina <99319593+aruzhanrakhimberdina@users.noreply.github.com>
2022-02-24 23:42:50 +0600

    Update Controller.java

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -4,14 +4,16 @@ public class Controller {
     private final IUserRepository repo;
 
     public Controller(IUserRepository repo) {
-        this.repo=repo;
+        this.repo=repo; //
     }
 
     public String createUser(String login,String password) {
-        User user=new User(login, password);
+        User user=new User(login, password); //allocating memory for the class User
 
-        boolean created=repo.createLogin(user);
+        boolean created=repo.createLogin(user); //an account is created and added to the database
 
         return ( created ? "User is created" : "User is not created");
+        //if account created, it is deduced "User is created"
+        // if it is not created, it is deduced "User is not created"
     }
 }
