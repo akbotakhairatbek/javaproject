78d628833ca63c9220fc6810b340cfe9f810a522
aruzhanrakhimberdina <99319593+aruzhanrakhimberdina@users.noreply.github.com>
2022-02-24 23:44:17 +0600

    Update Registration.java

--- src/com/company/Registration.java
+++ src/com/company/Registration.java
@@ -3,22 +3,22 @@ package com.company;
 import java.util.Scanner;
 
 public class Registration {
-    private final Controller contr;
+    private final Controller contr; //declaring an entity, that means that the value can't be modified in the future
     private Scanner sc;
 
     public Registration(Controller contr) {
         this.contr = contr;
         sc = new Scanner(System.in);
     }
-
+//In general here the work with the console is described
         public void loginStart() {
             System.out.println();
-            System.out.println("Welcome to Login Page");
-            System.out.println("Please enter email: ");
+            System.out.println("Welcome to Login Page"); //output to the console "Welcome to Login Page"
+            System.out.println("Please enter email: "); //output to the console "Please enter email: "
             String login=sc.nextLine();
-            System.out.println("Please enter password: ");
+            System.out.println("Please enter password: "); //output to the console "Please enter password: "
             String pass=sc.nextLine();
             String final1=contr.createUser(login,pass);
-            System.out.println("Thank you for signing in!");
+            System.out.println("Thank you for signing in!"); //output to the console "Thank you for signing in!"
     }
 }
