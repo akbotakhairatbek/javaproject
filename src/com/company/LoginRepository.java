152abdea2fb668dce00db5a06a12e4c2b93a6724
akbota <97677267+akbotakhairatbek@users.noreply.github.com>
2022-02-24 23:37:28 +0600

    Add files via upload

--- src/com/company/LoginRepository.java
+++ src/com/company/LoginRepository.java
@@ -6,5 +6,35 @@ import java.sql.SQLException;
 
 public class LoginRepository implements IUserRepository{
     private final IDB db;
-    
+
+    public LoginRepository(IDB db) {
+        this.db=db;
+    }
+
+    @Override
+    public boolean createLogin(User user) {
+        Connection con=null;
+        try {
+            con= db.getConnection();
+            String sql="insert into users(login, password) values (?,?)";
+            PreparedStatement stmt=con.prepareStatement(sql);
+
+            stmt.setString(1,user.getLogin());
+            stmt.setString(2,user.getPassword());
+
+            stmt.execute();
+            return true;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        return false;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	IDB db=new DBPostgres();
     IUserRepository rep=new LoginRepository(db);
     Controller contr=new Controller(rep);
     Registration reg=new Registration(contr);
