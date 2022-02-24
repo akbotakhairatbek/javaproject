bb314a3ab540f0c2dd2919999be17a2762c070cd
issatayeva <59280873+issatayeva@users.noreply.github.com>
2022-02-24 23:31:15 +0600

    Add files via upload
    connected to the database and added a login repository

---
+++ src/com/company/LoginRepository.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class LoginRepository implements IUserRepository{
+    private final IDB db;
+    
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+	IDB db=new DBPostgres();
+    IUserRepository rep=new LoginRepository(db);
     Controller contr=new Controller(rep);
     Registration reg=new Registration(contr);
     reg.loginStart();
